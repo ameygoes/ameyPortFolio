74ca6e0932b2277d5c56baedda2b9d3e35f9648c
Amey <bamey2241997@gmail.com>
2021-11-04 01:23:22 +0530

    Updates

---
+++ precache-manifest.ba6313620ed0d4a9f8f8fdb1147a9468.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219b84506d002871cc84df356df39857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e2f777c6731d050cd2e",
+    "url": "/static/css/3.e6756da0.chunk.css"
+  },
+  {
+    "revision": "40ac88205316baee2172",
+    "url": "/static/css/4.09b2f187.chunk.css"
+  },
+  {
+    "revision": "96792880fc97d49e95a6",
+    "url": "/static/css/main.346b450b.chunk.css"
+  },
+  {
+    "revision": "165ab0c25031bd03a8de",
+    "url": "/static/js/2.f58ec59f.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.f58ec59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2f777c6731d050cd2e",
+    "url": "/static/js/3.6d2ee356.chunk.js"
+  },
+  {
+    "revision": "40ac88205316baee2172",
+    "url": "/static/js/4.d5a38f56.chunk.js"
+  },
+  {
+    "revision": "96792880fc97d49e95a6",
+    "url": "/static/js/main.80253c1b.chunk.js"
+  },
+  {
+    "revision": "18191cb0d4b1355fbef2",
+    "url": "/static/js/runtime-main.96956f81.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "85857e487cacbd31dd4f551cba05b2a7",
+    "url": "/static/media/NPTEL.85857e48.jpg"
+  },
+  {
+    "revision": "7fa89e16967b93843982962f750cc7e0",
+    "url": "/static/media/PICTLogo.7fa89e16.jpeg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "4dee89baf42f2d7cd746292869d754e0",
+    "url": "/static/media/dbms.4dee89ba.jpg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "91653658f83451a8e31a2e6d6b18b7de",
+    "url": "/static/media/emoji.91653658.jpeg"
+  },
+  {
+    "revision": "8e3030f57b2b31d491c21e441a631cab",
+    "url": "/static/media/gcpLogo.8e3030f5.png"
+  },
+  {
+    "revision": "34228e33bf79c33bf418ceaf899d4147",
+    "url": "/static/media/ll.34228e33.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/static/media/python.b9bbb1d7.png"
+  },
+  {
+    "revision": "63d5fd003815ca6b4b0a861f39de3c16",
+    "url": "/static/media/pythondjango.63d5fd00.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "62b5a1b10a66f88d34319b6311346a9d",
+    "url": "/static/media/spclg.62b5a1b1.jfif"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
