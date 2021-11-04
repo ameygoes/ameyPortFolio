fed4e4fafb34842df4e5a929d5da26579cc6fd54
ameygoes <39159923+ameygoes@users.noreply.github.com>
2021-11-04 11:14:32 +0000

    Deploying to gh-pages from  @ 93390b7fe62184c1703a94f06b6dc2d8b5c361c3 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ameyportfolio/precache-manifest.51289dc6337a2bdeb7aeb47cb397239b.js"
+  "/precache-manifest.b1877103e0aaa47750b0f6a15ef73a77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ameyportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
